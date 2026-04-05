85d6a36ac3b1faecbfdcb50d6a885ee9f5ed55e5
ahmad <ahmedorfaly1982@gmail.com>
2026-04-05 09:22:46 +0100

    add logic to count words and print the 5 most frequent

--- app.py
+++ app.py
@@ -10,8 +10,21 @@ def get_words():
                             words[word] += 1
                         else:
                             words[word] = 1
-        print(words)
+        return words
     except FileNotFoundError:
         print("The file 'test.txt' was not found.")
 
-get_words()
+def print_result(all_the_words, the_top_5):
+    print("="*30)
+    print(f"There are {all_the_words} different words")
+    print("The top five word:")
+    print("="*30)
+    for word, frequency in the_top_5:
+        print(f"- {word:<5} {frequency:<2} times")
+    print("="*30)
+
+if __name__ == "__main__":
+    sorted_words = sorted(get_words().items(), key=lambda item: item[1], reverse=True)
+    all_words = len(sorted_words)
+    top_5 = sorted_words[:5]
+    print_result(all_words, top_5)
